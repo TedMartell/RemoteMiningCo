7ad9e2a3e4bfa2364e77539bc4d4cd2c30f83b41
Ŕickard Hellström <rickardhellstrom95@gmail.com>
2024-11-05 19:12:39 +0200

    Fixed Smelter

---
+++ scripts/data/smelterRecipes.js
@@ -0,0 +1,57 @@
+export const smelterRecipes = {
+	"Aluminium Ingot": {
+    	requiredItems: {Bauxite: 2},
+		output: {Aluminium_Ingot: 1},
+		smelting_time: 3
+    },
+    "Silicon": {
+       requiredItems: {Quartz: 10},
+	   output: {Silicon: 1},
+	   smelting_time: 12
+    },
+    "Iron Ingot": {
+       requiredItems: {Iron: 2},
+	   output: {Iron_Ingot: 1},
+	   smelting_time: 3
+    },
+    "Chromium Ingot": {
+       requiredItems: {Chromium: 2}, 
+	   output: {Chromium_Ingot: 1},
+	   smelting_time: 3
+    },
+    "Copper Ingot": {
+		requiredItems: {Copper: 2},
+		output: {Copper_Ingot: 1},
+		smelting_time: 3
+    },
+    "Lithium Ingot": {
+		requiredItems: {Lithium: 2},
+		output: {Lithium_Ingot: 1},
+		smelting_time: 3
+    },
+    "Silver Ingot": {
+        requiredItems: {Silver: 2}, 
+		output: {Silver_Ingot: 1},
+		smelting_time: 3
+    },
+    "Nickel Ingot": {
+		requiredItems: {Nickel: 2}, 
+		output: {Nickel_Ingot: 1},
+		smelting_time: 3
+    },
+    "Zinc Ingot": {
+		requiredItems: {Zinc: 2},
+		output: {Zinc_Ingot: 1},
+		smelting_time: 3
+	},
+	"Gold Ingot": {
+		requiredItems: {Gold: 2},
+		output: {Gold_Ingot: 1},
+		smelting_time: 3
+	},
+	"Steel Ingot": {
+		requiredItems: {Coal: 1, iron_ingot: 2},
+		output: {Diamond: 1},
+		smelting_time: 30
+	}
+};
