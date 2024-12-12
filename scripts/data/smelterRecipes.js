c76cbd4df465aae7589aed8b90cb4b991d767db6
Ted Martell <martellted@gmail.com>
2024-12-12 23:04:30 +0200

    Edge borders Basic Menu Smelter changes

--- scripts/data/smelterRecipes.js
+++ scripts/data/smelterRecipes.js
@@ -1,56 +1,56 @@
 export const smelterRecipes = {
 	"Aluminium Ingot": {
-    	requiredItems: {Bauxite: 2},
+    	requiredItems: {Bauxite: 2, coal: 5},
 		output: {Aluminium_Ingot: 1},
 		smelting_time: 3
     },
     "Silicon": {
-       requiredItems: {Quartz: 10},
+       requiredItems: {Quartz: 10, coal: 5},
 	   output: {Silicon: 1},
 	   smelting_time: 12
     },
     "Iron Ingot": {
-       requiredItems: {Iron: 2},
+       requiredItems: {Iron: 2, coal: 5},
 	   output: {Iron_Ingot: 1},
 	   smelting_time: 3
     },
     "Chromium Ingot": {
-       requiredItems: {Chromium: 2}, 
-	   output: {Chromium_Ingot: 1},
+       requiredItems: {Chromium: 2, coal: 5}, 
+	   output: {Chromium_Ingot: 1, coal: 5},
 	   smelting_time: 3
     },
     "Copper Ingot": {
-		requiredItems: {Copper: 2},
+		requiredItems: {Copper: 2, coal: 5},
 		output: {Copper_Ingot: 1},
 		smelting_time: 3
     },
     "Lithium Ingot": {
-		requiredItems: {Lithium: 2},
+		requiredItems: {Lithium: 2, coal: 5},
 		output: {Lithium_Ingot: 1},
 		smelting_time: 3
     },
     "Silver Ingot": {
-        requiredItems: {Silver: 2}, 
+        requiredItems: {Silver: 2, coal: 5}, 
 		output: {Silver_Ingot: 1},
 		smelting_time: 3
     },
     "Nickel Ingot": {
-		requiredItems: {Nickel: 2}, 
+		requiredItems: {Nickel: 2, coal: 5}, 
 		output: {Nickel_Ingot: 1},
 		smelting_time: 3
     },
     "Zinc Ingot": {
-		requiredItems: {Zinc: 2},
+		requiredItems: {Zinc: 2, coal: 5},
 		output: {Zinc_Ingot: 1},
 		smelting_time: 3
 	},
 	"Gold Ingot": {
-		requiredItems: {Gold: 2},
+		requiredItems: {Gold: 2, coal: 5},
 		output: {Gold_Ingot: 1},
 		smelting_time: 3
 	},
 	"Steel Ingot": {
-		requiredItems: {Coal: 1, iron_ingot: 2},
+		requiredItems: {Coal: 10, iron_ingot: 2},
 		output: {Diamond: 1},
 		smelting_time: 30
 	}
