41007c87226ea87351fd37b297b7d54171daf46c
Ted Martell <martellted@gmail.com>
2025-01-20 22:14:51 +0200

    Sell crates all have x10 and all function

--- scripts/data/smelterRecipes.js
+++ scripts/data/smelterRecipes.js
@@ -7,47 +7,47 @@ export const smelterRecipes = {
     "Silicon": {
        requiredItems: {Quartz: 4, Coal: 2},
 	   output: {Silicon: 1},
-	   smelting_time: 5
+	   smelting_time: 3
     },
     "Iron Ingot": {
-       requiredItems: {Iron: 2, Coal: 5},
+       requiredItems: {Iron: 5, Coal: 2},
 	   output: {Iron_Ingot: 1},
-	   smelting_time: 3
+	   smelting_time: 5
     },
     "Chromium Ingot": {
-       requiredItems: {Chromium: 2, Coal: 2}, 
+       requiredItems: {Chromium: 5, Coal: 2}, 
 	   output: {Chromium_Ingot: 1},
-	   smelting_time: 3
+	   smelting_time: 5
     },
     "Copper Ingot": {
-		requiredItems: {Copper: 2, Coal: 2},
+		requiredItems: {Copper: 5, Coal: 2},
 		output: {Copper_Ingot: 1},
-		smelting_time: 3
+		smelting_time: 5
     },
     "Lithium Ingot": {
-		requiredItems: {Lithium: 2, Coal: 2},
+		requiredItems: {Lithium: 6, Coal: 3},
 		output: {Lithium_Ingot: 1},
-		smelting_time: 3
+		smelting_time: 10
     },
     "Silver Ingot": {
-        requiredItems: {Silver: 2, Coal: 2}, 
+        requiredItems: {Silver: 6, Coal: 3}, 
 		output: {Silver_Ingot: 1},
-		smelting_time: 3
+		smelting_time: 10
     },
     "Nickel Ingot": {
-		requiredItems: {Nickel: 2, Coal: 2}, 
+		requiredItems: {Nickel: 6, Coal: 3}, 
 		output: {Nickel_Ingot: 1},
-		smelting_time: 3
+		smelting_time: 10
     },
     "Zinc Ingot": {
-		requiredItems: {Zinc: 2, Coal: 2},
+		requiredItems: {Zinc: 8, Coal: 4},
 		output: {Zinc_Ingot: 1},
-		smelting_time: 3
+		smelting_time: 30
 	},
 	"Gold Ingot": {
-		requiredItems: {Gold: 2, Coal: 2},
+		requiredItems: {Gold: 10, Coal: 5},
 		output: {Gold_Ingot: 1},
-		smelting_time: 3
+		smelting_time: 45
 	},
 	"Steel Ingot": {
 		requiredItems: {Coal: 10, iron_ingot: 2},
